2280a0b6345ca0f7cdc2ea9fa485a9bc1ac895a9
pradeeptodixit <pradeeptodixit9@gmail.com>
2026-04-06 21:30:44 +0530

    Experiments

---
+++ Bootstrap/console.js
@@ -0,0 +1,18 @@
+let name="Pradeepto Dixit";
+let age=19;
+var isEmployed= true;
+console.log(name);
+console.log(age);
+console.log(isEmployed);
+if (true) 
+    {
+    let age=30;
+}
+console.log(age);
+
+// Therefore we should not use var !
+let greeting="Hello " + name + "! You are "+age+" years old." ;
+console.log(greeting)
+// OR
+// let greeting=  `Hello ${name} ! You are ${age} years old.`;
+// console.log (greeting) ;This
---
+++ Bootstrap/primenumber.js
@@ -0,0 +1,23 @@
+// Prime Numbers
+function isPrime(num) 
+{
+    if (num<=1) {
+        return false;
+    }
+    if (num===2) {
+        return true;
+    }
+    for (let i=2; i<num; i++) {
+        if (num%i===0) {
+            return false;
+        }
+    }
+    return true;
+}
+let number=14;
+if (isPrime(number)) {
+    console.log(`${number} is a prime number.`)
+}
+else {
+    console.log(`${number} is not a prime number.`)
+}
